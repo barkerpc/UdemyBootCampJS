814fd2ddd95277b60e40e3db84cf26808344002e
barker <barkerpc@gmail.com>
2018-10-19 22:52:47 +0000

    On to Booleans

---
+++ basics/boolean.js
@@ -0,0 +1,12 @@
+/// is the equality operator
+/// != not equal operator
+// < - less than operator
+// > - greater than operator
+// >= greater than or equal to operator
+// <= less than oro equal to operator
+
+let temp = 31;
+let isFreezing = temp <= 31;
+
+
+console.log(isFreezing)
