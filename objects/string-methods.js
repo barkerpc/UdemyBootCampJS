fb034af0fb2d3ec9a67f640052713383ce09d267
Paul Barker <barkerpc@gmail.com>
2019-02-21 18:53:12 -0500

    stringmethods

---
+++ objects/string-methods.js
@@ -0,0 +1,18 @@
+let name = ' Paul Barker '
+
+// log the length
+console.log(name.length)
+
+// convert to upper case
+console.log(name.toUpperCase())
+
+console.log(name.toLowerCase())
+
+console.log(name.includes('Paul',0))
+
+
+let password = 'abc123password098'
+
+console.log(password.includes('password'))
+
+console.log(name.trim())
