d3757250e420d679d9e00f1ff4d288ef230f3de7
Paul Barker <barkerpc@gmail.com>
2019-03-15 19:02:16 -0400

    number methods

---
+++ objects/number-methods.js
@@ -0,0 +1,9 @@
+let num = 103.941
+
+console.log(num.toFixed(2))
+
+// Math based methods
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+console.log(Math.random())
