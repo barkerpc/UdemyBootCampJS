1363f29b5913908482432b45c3f51d43268331bd
Paul Barker <barkerpc@gmail.com>
2019-03-15 19:09:25 -0400

    simple number guesser

--- objects/number-methods.js
+++ objects/number-methods.js
@@ -7,3 +7,27 @@ console.log(Math.round(num))
 console.log(Math.floor(num))
 console.log(Math.ceil(num))
 console.log(Math.random())
+
+let min = 1
+let max = 5
+// random num between min and max
+//let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+
+console.log(randomNum)
+
+
+//1-5 true if correct
+let makeGuess = function(numberGuess) {
+    
+    let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+    
+    if (numberGuess === randomNum) {
+        return true
+    } else {
+        return false
+    }
+
+
+}
+
+console.log(makeGuess(1))
