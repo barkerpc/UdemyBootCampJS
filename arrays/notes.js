fbb2a90a58ef008e18bbfbdfd5af9047d69f79df
Paul Barker <barkerpc@gmail.com>
2019-09-04 18:23:11 -0400

    arrays searching part 1

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,21 +1,34 @@
-const notes = ['This is note 1', 'This is note 2', 'This is note 3']
+const notes = [{},{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Things to work on',
+    body: 'Exersize, eating better'
+} , {
+     title: 'Office modifications',
+     body : 'Get a new seat'
+}]
 
-console.log(notes.pop())
-notes.push('This is note 4')
+// console.log(notes.pop())
+// notes.push('This is note 4')
 
-console.log(notes.shift())
-console.log(notes.unshift('This is note 0'))
+// console.log(notes.shift())
+// console.log(notes.unshift('This is note 0'))
 
 // notes.splice(1,1)
 //notes.splice(1,0,'This is note mid')
-notes[2] = 'Note monkey'
+// notes[2].title = 'Note monkey'
 
-notes.forEach(function(item,index) {
-    console.log(item)
-    console.log(index)
-})
+// notes.forEach(function(item,index) {
+//     console.log(item)
+//     console.log(index)
+// })
 
 console.log(notes.length)
 console.log(notes)
+const index  = notes.findIndex(function (note,index) {
+    //console.log(note)
+    return note.title === 'Things to work on'
 
-console.log (notes.indexOf('This is note 2'))
+})
+console.log(index)
