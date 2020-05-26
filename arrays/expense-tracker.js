8ac7b4c0d554195b1cfb4b196a17863680be1a36
Paul Barker <barkerpc@gmail.com>
2020-05-26 19:19:10 -0400

    finish basic expense tracker

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -21,28 +21,22 @@ const account = {
     getAccountSummary: function () {
         let totalExpenses = 0
         let totalIncome = 0
+        let balance = 0
         this.expenses.forEach(function (expense) {
-            totalExpenses = totalExpenses + expense.amount
-           
+            totalExpenses = totalExpenses + expense.amount           
 
         }),
         this.income.forEach(function (income) {
-            totalIncome = totalIncome + income.amount
-           
+            totalIncome = totalIncome + income.amount           
 
         })
-        let balance = totalIncome - totalExpenses
+        balance = totalIncome - totalExpenses
         return `${this.name} has a balance of $${balance}. $${totalExpenses} in expenses, and $${totalIncome} income`
     }
 }
-
-// 1. Add an income array to account
-// 2. Setup addIncome method  --> description and amount
-// 3. Tweak getAccountSummary to get the balance
 
 account.addIncome('Pay check',1950)
 account.addIncome('Bribes', 200)
 account.addExpense('Rent',950)
 account.addExpense('Coffe', 2)
-//console.log(account)
 console.log(account.getAccountSummary())
